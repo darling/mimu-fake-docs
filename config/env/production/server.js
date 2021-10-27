c32e6f800fe8b76a82f7c8cb3552b1d0d4600f9f
carter <safe@unworthy.net>
2021-10-27 00:54:57 -0700

    bruh

---
+++ config/env/production/server.js
@@ -0,0 +1,5 @@
+module.exports = ({ env }) => ({
+  url: env(
+    "postgres://livizeyeubwlie:c535a5f1e51d2e0445a4d1b2d89e4caca9bbc796c66877bd2542be5c30378cdc@ec2-107-23-135-132.compute-1.amazonaws.com:5432/d5k247eio3iiiv"
+  ),
+});
