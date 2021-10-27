a145f36480ae4e4816cb6d7f676d325c3c46bdce
carter <safe@unworthy.net>
2021-10-27 01:05:09 -0700

    arr

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,3 @@
 module.exports = ({ env }) => ({
-  url: env(
-    "postgres://livizeyeubwlie:c535a5f1e51d2e0445a4d1b2d89e4caca9bbc796c66877bd2542be5c30378cdc@ec2-107-23-135-132.compute-1.amazonaws.com:5432/d5k247eio3iiiv"
-  ),
+  url: env("MY_HEROKU_URL"),
 });
